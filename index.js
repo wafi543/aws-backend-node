ed406e7a95b2d721a2da8a002fb696ac4c811903
walshammari <walshammari@site.sa>
2023-07-31 20:00:51 +0300

    init node express, add pg

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const bodyParser = require('body-parser')
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {require: true, rejectUnauthorized: false}
+})
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+const getStudents = (request, response) => {
+  pool.query('select * from students', (error, results) => {
+    if (error) {
+      response.status(500).send(error);
+      return;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getGroups = (request, response) => {
+  pool.query('select * from students where is_leader = true', (error, results) => {
+    if (error) {
+      response.status(500).send(error);
+      return;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+app.get('/api/students', getStudents)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
